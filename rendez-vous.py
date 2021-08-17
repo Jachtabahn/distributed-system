e69122f77b1a8bba49df798020ce9053b111176a
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2021-08-17 18:29:49 +0200

    Rendez Vous Server

--- rendez-vous.py
+++ rendez-vous.py
@@ -1,17 +1,32 @@
 import tornado.ioloop
 import tornado.web
 
+ips = []
+
 class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        client_information = str(self.request.connection.stream.socket)
-        self.write("Hello, world: " + client_information[1:-1])
+
+  def get(self):
+    # We use an object with only one value that is our list because of this error message:
+    # TypeError: write() only accepts bytes, unicode, and dict objects. Lists not accepted for security reasons; see http://www.tornadoweb.org/en/stable/web.html#tornado.web.RequestHandler.write
+    self.write({"ips":ips})
+
+  def post(self):
+    visible_address_pair = self.request.connection.stream.socket.getpeername()
+    invisible_address = self.request.body.decode()
+    visible_address = "{}:{}".format(*visible_address_pair)
+
+    ips.append(invisible_address)
+    ips.append(visible_address)
+    print(invisible_address)
+    print(visible_address)
+    print(ips)
 
 def make_app():
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
+  return tornado.web.Application([
+    (r"/", MainHandler),
+  ])
 
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+  app = make_app()
+  app.listen(8888)
+  tornado.ioloop.IOLoop.current().start()
